5b193085462770b93f61710c7887862edcd4cff1
Sulta <Sulta@CESAR_ED_PEA>
2023-08-30 21:34:09 -0500

    Agregar archivos de proyecto.

---
+++ PROYECTO_LPOO_2023.2/PROPUESTAS_DE_PROYECTO.cpp
@@ -0,0 +1,17 @@
+/*
+PROPUESTAS DE PROYECTO:
+1era propuesta: Sistema de brazo robótico que permita discernir que tipo de objeto (metal, plastico, madera,etc)
+es el que está agarrando, dependiendo del tipo de objeto procederá a ubicarlo en el basurero respectivo de su tipo.
+
+
+2da propuesta: Sistema de identificador de alumno PUCP, donde de acuerdo al código del alumno permita verificar
+mediante un reconocimiento facial que la persona que colocó el código en el sistema es el alumno con dicho código.
+(podrían añadirse alarmas u otro dispositivo de alerta en el caso de que no se verifique el reconocimiento facial)
+
+
+3era propuesta: Sistema encargado de verificar que el código del alumno se encuentre dentro de la base de datos,
+una vez hecha la verificación, el sistema ofrece un ticket(puede ser otro objeto) el cual en otra estación el ticket
+será verificado por el sistema, dandole opción a elegir el plato y bebida que desee, esto podrá ser controlado a través
+de botones switches los cuales permitiran que el plato o bebida sea llevado al lugar donde uno se encuentre por medio
+de una faja transportadora
+*/
