3b5795e966fbb70dc527f8dfe5f41d2e2dcdf6c7
diego <diego@LAPTOP-4HPPDAV3>
2023-09-06 02:37:18 -0500

    Gestion

---
+++ Archivo de origen/Gestion.cpp
@@ -0,0 +1,19 @@
+#include "Gestion.h"
+
+Gestion::Gestion(){
+	//Constructor
+}
+
+Gestion::Gestion(int codigo, String^ nom_ruta, int cant_usua_time, int cant_usua_ruta,
+	         int cant_usua_par, String^ nom_parada, String^ ID_empleado, double media_califica) {
+
+	this->codigo = codigo;
+	this->nom_ruta = nom_ruta;
+	this->cant_usua_time = cant_usua_time;
+	this->cant_usua_ruta = cant_usua_ruta;
+	this->cant_usua_par = cant_usua_par;
+	this->nom_parada = nom_parada;
+	this->ID_empleado = ID_empleado;
+	this->media_califica = media_califica;
+
+}
--- Archivos de encabezado/Gestion.h
+++ Archivos de encabezado/Gestion.h
@@ -1,1 +1,22 @@
 #pragma once
+
+
+using namespace System;
+
+public ref class Gestion {
+private:
+
+	int codigo;
+	String^ nom_ruta;
+	int cant_usua_time;
+	int cant_usua_ruta;
+	int cant_usua_par;
+	String^ nom_parada;
+	String^ ID_empleado;
+	double media_califica;
+
+
+public:
+	Gestion();
+	Gestion(int codigo,String^ nom_ruta,int cant_usua_time,int cant_usua_ruta,int cant_usua_par,String^ nom_parada,String^ ID_empleado,double media_califica);
+};
