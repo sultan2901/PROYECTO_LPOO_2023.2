edf2e28d80c08cfb62c9ccefcb2ea7616f3432b4
diego <diego@LAPTOP-4HPPDAV3>
2023-09-06 01:51:01 -0500

    Calificacion

---
+++ Archivo de origen/Calificacion.cpp
@@ -0,0 +1,13 @@
+#include "Calificacion.h"
+
+Calificacion::Calificacion() {
+	//Constructor
+}
+
+Calificacion::Calificacion(int  codigo, int numero, int destino) {
+	this->codigo = codigo;
+	this->numero = numero;
+	this->destino = destino;
+	
+}
+
--- Archivos de encabezado/Calificacion.h
+++ Archivos de encabezado/Calificacion.h
@@ -1,1 +1,16 @@
 #pragma once
+
+using namespace System;
+
+
+public ref class Calificacion {
+	private:
+		int  codigo;
+		int numero;
+		int destino;
+	public:
+		Calificacion();
+		Calificacion(int  codigo, int numero, int destino);
+	
+
+};
