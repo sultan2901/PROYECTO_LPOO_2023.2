e84282e26f9d77d12d70a12bbac347e404871eb4
diego <diego@LAPTOP-4HPPDAV3>
2023-09-06 02:00:56 -0500

    estadistica

---
+++ Archivo de origen/Estadistica.cpp
@@ -0,0 +1,19 @@
+#include "Estadistica.h"
+
+Estadistica::Estadistica(){
+	//Constructor
+}
+
+Estadistica::Estadistica(int codigo, double media, double moda, double mediana, double Maximo, double Minimo, double desvia) {
+
+	this->codigo = codigo;
+	this->media = media;
+	this->moda = moda;
+	this->mediana = mediana;
+	this->Maximo = Maximo;
+	this->Minimo = Minimo;
+	this->desvia = desvia;
+		
+	
+
+}
--- Archivos de encabezado/Estadistica.h
+++ Archivos de encabezado/Estadistica.h
@@ -1,1 +1,19 @@
 #pragma once
+
+using namespace System;
+
+public ref class Estadistica {
+private:
+	int codigo;
+	double media;
+	double moda;
+	double mediana;
+	double Maximo;
+	double Minimo;
+	double desvia;
+
+
+public:
+	Estadistica();
+	Estadistica(int codigo,double media,double moda,double mediana,double Maximo,double Minimo,double desvia);
+};
