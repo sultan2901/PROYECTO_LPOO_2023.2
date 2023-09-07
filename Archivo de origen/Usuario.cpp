635effc85b0aa459a5f24a719786e63f0b4637d4
diego <diego@LAPTOP-4HPPDAV3>
2023-09-07 18:20:55 -0500

    hola

--- Archivo de origen/Usuario.cpp
+++ Archivo de origen/Usuario.cpp
@@ -4,12 +4,11 @@ Usuario::Usuario() :Persona() {
 
 };
 
-Usuario::Usuario(int Codigo, String^ Nombres, String^ ApellidoPat, String^ ApellidoMat, String^ DNI, int Edad, String^ Genero, String^ Telefon, String^ FechaNacimiento. double X_Usario, double Y_Usuario, String^ Contrasena, bool Registrado, Tarjeta^ ObjTarjeta, DatosPromocionar^ ObjDatosPromocionar) :Persona(Codigo, Nombres, ApellidoPat, ApellidoMat, DNI, Edad, Genero, Telefono) {
+Usuario::Usuario(int Codigo, String^ Nombres, String^ ApellidoPat, String^ ApellidoMat, String^ DNI, int Edad, String^ Genero, String^ Telefon, String^ FechaNacimiento. double X_Usario, double Y_Usuario, bool Registrado, Tarjeta^ ObjTarjeta, DatosPromocionar^ ObjDatosPromocionar) :Persona(Codigo, Nombres, ApellidoPat, ApellidoMat, DNI, Edad, Genero, Telefono) {
 
 	this->FechaNacimiento = FechaNacimiento;
 	this->X_Usuario = X_Usuario;
 	this->Y_Usuario = Y_Usuario;
-	this->Contrasena = Contrasena;
 	this->Registrado = Registrado;
 	this->ObjTarjeta = ObjTarjeta;
 	this->ObjDatosPromocionar = ObjDatosPromocionar;
